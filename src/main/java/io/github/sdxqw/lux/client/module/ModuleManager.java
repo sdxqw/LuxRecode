e7f61f529082f5caa4caa83a9384701999b95735
xNtrow <xntrowdev@gmail.com>
2022-09-01 00:03:46 +0200

    Auto add modules lol

--- src/main/java/io/github/sdxqw/lux/client/module/ModuleManager.java
+++ src/main/java/io/github/sdxqw/lux/client/module/ModuleManager.java
@@ -1,33 +1,27 @@
 package io.github.sdxqw.lux.client.module;
 
 import com.google.common.collect.Lists;
-import io.github.sdxqw.lux.client.module.features.TestMod;
-import io.github.sdxqw.lux.client.module.features.TestModNormal;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import org.reflections.Reflections;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 
 @Getter
 public class ModuleManager {
 
+    private final Set<Class<?>> classes;
     protected List<ModuleBase> mods = Lists.newArrayList();
     protected String toCheck = "io.github.sdxqw.lux.client.module.features";
-    private final Set<Class<?>> classes;
 
+    @SneakyThrows
     public ModuleManager() {
         Reflections reflection = new Reflections(toCheck);
         classes = reflection.getTypesAnnotatedWith(ModuleInfo.class);
-        addModule(
-                new TestMod(),
-                new TestModNormal()
-        );
-    }
-
-    private void addModule(ModuleBase... modules) {
-        mods.addAll(Arrays.asList(modules));
+        for (Class<?> e : classes) {
+            mods.add((ModuleBase) e.newInstance());
+        }
     }
 
     public void renderHooks() {
